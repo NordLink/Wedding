efa66cc4596290e117d6b6d4434f09fa37735898
Nord <alexplfg@gmail.com>
2024-06-04 14:01:32 +0300

    Fixed header when scrolling

--- src/header.js
+++ src/header.js
@@ -3,7 +3,8 @@ const header = document.querySelector('.header')
 const changeHeaderStyles = () => {
     if (window.scrollY > 100) {
         header.classList.add('fixed')
-    } else {
+    }
+    if(window.scrollY == 0) {
         header.classList.remove('fixed')
     }
 }
